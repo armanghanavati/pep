fa07d53862745abd702c3c35b9456c54994244b1
unknown <akhlaghimo@yahoo.com>
2023-09-13 12:13:23 +0330

    change site address

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ window.apiAddress="http://172.16.1.40:2884/api";
 //window.apiAddress="http://localhost:7142/api"; 
 
 //  window.siteAddress="http://coapp:9191";
- window.siteAddress="http://localhost:7142";
+ //window.siteAddress="http://localhost:7142";
+ window.siteAddress="http://172.16.1.40:2884";
 
  window.confirmPayment="http://172.16.1.40:2884/api"
 //  window.confirmPayment="http://localhost:7142/api"
