b16093d3a5f0d3d539315f4f4323357ec1b76d97
Akhlaghi Mohamadreza <akhlaghi.mo@minoo.com>
2023-11-27 12:43:51 +0330

    change index

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import "./assets/fonts/IRANSansWeb.ttf";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter } from "react-router-dom";
 
-
+//window.apiAddress="http://localhost:7142/api";
 //window.apiAddress="http://localhost:7021/api";  
 window.apiAddress="http://172.16.1.40:3884/api"; //peppay
 // window.apiAddress="http://172.16.1.186:2884/api";   //coappweb
