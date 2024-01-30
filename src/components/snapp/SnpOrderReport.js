64967094d5fb23ddceee86633b40f4c0f4691f25
Akhlaghi Mohamadreza <akhlaghi.mo@minoo.com>
2024-01-30 12:30:53 +0330

    refactor snap order

---
+++ src/components/snapp/SnpOrderReport.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SnpOrderReport extends React.Component {
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+
+    async componentDidMount() {  
+    }
+}
+
+export default SnpOrderReport;
--- src/index.js
+++ src/index.js
@@ -14,13 +14,14 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter } from "react-router-dom";
 
 window.apiAddress="http://localhost:7142/api";  
-
+//window.apiAddess="http://172.16.1.40:3884/api"
 //window.apiAddress="https://pepapi.minoomart.ir/api";
-window.siteAddress = "https://pepapi.minoomart.ir";
+//window.siteAddress = "https://pepapi.minoomart.ir";
 // window.siteAddress = "http://172.16.1.40:2884";
-window.confirmPayment = "https://pepapi.minoomart.ir/api";
+//window.siteAddress = "http://172.16.1.40:3884";
+//window.confirmPayment = "https://pepapi.minoomart.ir/api";
 //window.apiAddressInspection="https://pepinspectionapi.minoomart.ir/api";
-window.apiAddressInspection="http://localhost:7021/api";  
+//window.apiAddressInspection="http://localhost:7021/api";  
 //window.apiAddress="http://172.16.1.186:2884/api";   //coappweb
 
 // const store = createStore(rootReducer);
