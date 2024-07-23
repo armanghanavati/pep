854b5dfe74d0ea96d2da371fa822c6cbb33f6ad7
Akhlaghi Mohamadreza <akhlaghi.mo@minoo.com>
2024-07-23 09:11:41 +0330

    change company and location config datagrid

--- src/components/location/Location-config.js
+++ src/components/location/Location-config.js
@@ -1,57 +1,7 @@
 export const DataGridLocationColumns = [
   {
-    dataField: "code",
-    caption: "کد",
-    allowEditing: false,
-  },
-  {
     dataField: "locationName",
     caption: "نام محل",
     allowEditing: false,
   },
-  {
-    dataField: "persianName",
-    caption: "نام فارسی",
-    allowEditing: false,
-  },
-  {
-    dataField: "locationTop",
-    caption: "زیر گروه محل",
-    allowEditing: false,
-  },
-  {
-    dataField: "stateName",
-    caption: "استان",
-    allowEditing: false,
-  },
-  {
-    dataField: "cityName",
-    caption: "شهر",
-    allowEditing: false,
-  },
-  {
-    dataField: "desc",
-    caption: "توضیحات",
-    allowEditing: false,
-  },
-  {
-    dataField: "locationType",
-    caption: "نوع محل",
-    allowEditing: false,
-  },
-  {
-    dataField: "company",
-    caption: "شرکت",
-    allowEditing: false,
-  },
-  {
-    dataField: "isActive",
-    caption: "فعال",
-    allowEditing: false,
-  },
-  {
-    dataField: "grade",
-    caption: "رتبه",
-    allowEditing: false,
-  }
 ];
