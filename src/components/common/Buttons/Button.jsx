1981b72d41e0d6841c71308dd3d8398a2d988597
armangh <armangh131313@gmail.com>
2024-07-27 17:47:09 +0330

    fix: buttons

--- src/components/common/Buttons/Button.jsx
+++ src/components/common/Buttons/Button.jsx
@@ -24,7 +24,7 @@ const index = ({
       width={120}
       text={text}
       type={type}
-      stylingMode="contained"
+      stylingMode={stylingMode}
       onClick={onClick}
       style={style}
     >
