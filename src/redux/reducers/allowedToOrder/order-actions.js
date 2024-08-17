508163ea57c8b572bd5c77e9f6b99c471715f940
armangh <armangh131313@gmail.com>
2024-08-17 10:48:22 +0330

    starting allowed order

---
+++ src/redux/reducers/allowedToOrder/order-actions.js
@@ -0,0 +1,8 @@
+// import axios from "axios";
+
+// export const updateItemLocationGroup = async (postData) => {
+//   const url = window.apiAddress + `/ItemLocation/updateItemLocationGroup`;
+//   const response = await axios.patch(url, postData);
+//   console.log(response);
+//   return response?.data;
+// };
